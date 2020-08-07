c11bfbff030e2b848100b14f1b36cecc2853babc
Nick <logikfx@gmail.com>
2020-08-06 20:56:34 -0400

    Refactored location of controllers, added new controller.  Closes #7.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package controllers;
+package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
+    @RequestMapping(value = {"", "/", "/index", "index.html"})
     public String index() {
         return "index";
     }
